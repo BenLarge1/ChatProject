53a4db2998e52803f3a771f76e56915c923b6e83
User <user@itic-osx-006.local>
2019-02-06 14:01:42 -0600

    committing

---
+++ CapstoneProject/StartPageViewController.swift
@@ -0,0 +1,37 @@
+//
+//  StartPageViewController.swift
+//  CapstoneProject
+//
+//  Created by User on 2/6/19.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+import UIKit
+
+class StartPageViewController: UIViewController {
+
+	//MARK Properties
+	
+	@IBOutlet weak var emailTextField: UITextField!
+	@IBOutlet weak var passwordTextField: UITextField!
+	
+	override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
