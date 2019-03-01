b5981f9e193503dec4b8db90a33276322b6855c7
Ben Large <benlarge1@gmail.com>
2019-02-28 19:18:15 -0600

    writes to firebase server, do not use as does not fully work

--- CapstoneProject/ReportBullyingFormViewController.swift
+++ CapstoneProject/ReportBullyingFormViewController.swift
@@ -7,10 +7,16 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseDatabase
+//import FirebaseStorage MARK: Need to install firebase storage
 
 class ReportBullyingFormViewController: UIViewController {
 
-    override func viewDidLoad() {
+    var ref = Database.database().reference()
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
@@ -18,19 +24,43 @@ class ReportBullyingFormViewController: UIViewController {
     
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var anonSwitch: UISwitch!
+    @IBOutlet weak var dateTextField: UITextField!
+    
     @IBAction func toggleSwitch(_ sender: UISwitch)
     {
         if anonSwitch.isOn
         {
             nameTextField.isUserInteractionEnabled = false
-			
+			nameTextField.backgroundColor = #colorLiteral(red: 0.2470588235, green: 0.5411764706, blue: 0.2392156863, alpha: 1)
+			nameTextField.textColor = #colorLiteral(red: 0.2470588235, green: 0.5411764706, blue: 0.2392156863, alpha: 1)
+			nameTextField.attributedPlaceholder = NSAttributedString(string: "", attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
         }
         else
         {
             nameTextField.isUserInteractionEnabled = true
-
+			nameTextField.backgroundColor = UIColor.white
+			nameTextField.textColor = UIColor.black
+			nameTextField.attributedPlaceholder = NSAttributedString(string: "Your Name", attributes: [NSAttributedStringKey.foregroundColor: UIColor.lightGray])
         }
     }
+	@IBOutlet weak var reportDescriptionTextView: UITextView!;
+	
+	let user = Auth.auth().currentUser
+
+	@IBAction func writeReportToDatabase(_ sender: UIButton) //writes their name (if applicable), date, and description to database (hopefully)
+    {
+		
+		
+		
+		self.ref.child("reports/" + (user?.uid)!).setValue(["report text": reportDescriptionTextView.text])
+		self.ref.child("reports/" + (user?.uid)!).setValue(["date": dateTextField.text])
+        
+        if !anonSwitch.isOn
+        {
+			self.ref.child("reports/" + (user?.uid)!).setValue(["name": nameTextField.text])
+        }
+    }
+    
     
     /*
     // MARK: - Navigation
