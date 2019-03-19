deffd17e2c6c045af75dd0888abe011ff17307e8
User <user@itic-osx-002.local>
2019-03-18 19:28:33 -0500

    finished logic on spotify and youtube buttons

--- CapstoneProject/MusicViewController.swift
+++ CapstoneProject/MusicViewController.swift
@@ -10,13 +10,38 @@ import UIKit
 
 class MusicViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func launchSpotifyPlaylistOne(_ sender: UIButton)
+    {
+        let settingsUrl = NSURL(string: "https://open.spotify.com/playlist/75ARDXD31P1pM0jtVmc8eZ")! as URL
+        UIApplication.shared.open(settingsUrl, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func launchSpotifyPlaylistTwo(_ sender: UIButton)
+    {
+        let settingsUrl = NSURL(string: "https://open.spotify.com/playlist/4dJ8Ys2bOgWQIRvs7QSkeE")! as URL
+        UIApplication.shared.open(settingsUrl, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func launchYouTubePlaylistOne(_ sender: UIButton)
+    {
+        let settingsUrl = NSURL(string: "https://www.youtube.com/playlist?list=PLBhIAkl3-Iovo1Bbw-k_NXGus-RnG2FtN")! as URL
+        UIApplication.shared.open(settingsUrl, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func launchYouTubePlaylistTwo(_ sender: UIButton)
+    {
+        let settingsUrl = NSURL(string: "https://www.youtube.com/playlist?list=PLBhIAkl3-Iovud5DyiASzw83S6d0fwOT2")! as URL
+        UIApplication.shared.open(settingsUrl, options: [:], completionHandler: nil)
+    }
+    
+    
     /*
     // MARK: - Navigation
 
